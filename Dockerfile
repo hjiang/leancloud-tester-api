b5b577b2122c88b545f7a0bb2d5407fe3c178d4a
Hong Jiang <j@1byte.io>
2019-01-03 20:04:42 +0800

    :lock: support https

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /app/
 COPY package-lock.json package.json tsconfig.json ./
 COPY src ./src
 RUN npm install && npm run build
-ENV PORT=4000
-EXPOSE ${PORT}
+ENV HTTP_PORT=4000
+ENV HTTPS_PORT=4040
+EXPOSE ${HTTP_PORT}
+EXPOSE ${HTTPS_PORT}
 CMD node lib/index.js
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import https from 'https';
+import fs from 'fs';
 import compression from "compression";  // compresses requests
 import bodyParser from "body-parser";
 var cors = require('cors')
@@ -47,7 +49,18 @@ app.get('/api/tests/:name/results/', async (req, res) => {
   res.json(jsonResponse);
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.HTTP_PORT || 4000;
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  console.log(`HTTP listening on port ${port}`)
 });
+
+if (process.env.SSL_KEY && process.env.SSL_CERT) {
+  const options = {
+    key: fs.readFileSync(process.env.SSL_KEY),
+    cert: fs.readFileSync(process.env.SSL_CERT)
+  };
+  const httpsPort = process.env.HTTPS_PORT || 4040;
+  https.createServer(options, app).listen(httpsPort, () => {
+    console.log(`HTTPS listening on port ${httpsPort}`);
+  });
+}
